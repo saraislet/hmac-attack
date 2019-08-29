83f77580d481aec01a4dcbd558c94920351899b8
Sarai Rosenberg <srosenberg@pagerduty.com>
2019-08-28 23:18:29 -0700

    Write nonconstant_compare

--- hmac_timing.py
+++ hmac_timing.py
@@ -10,6 +10,10 @@ def compute_hmac(secret, message):
     h.update(message)
     return h.hexdigest()
 
+def nonconstant_compare(a, b):
+    return a == b
+
 if __name__ == '__main__':
     print(compute_hmac(SECRET, MESSAGE))
+    print(nonconstant_compare('hihello', MESSAGE))
 
