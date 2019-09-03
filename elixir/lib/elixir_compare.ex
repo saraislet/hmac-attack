652b0cca4657afd62a0e8a483513aceed53787b4
Sophie Schmieg <sophie.schmieg@gmail.com>
2019-09-02 17:07:15 -0700

    Pretty constant time compare

--- elixir/lib/elixir_compare.ex
+++ elixir/lib/elixir_compare.ex
@@ -14,16 +14,31 @@ defmodule ElixirCompare do
     repeat_compare(target, guess, count - 1)
   end
 
+  def repeat_constant_time_compare(target, guess, count) when count <= 1 do
+    constant_time_compare(target, guess)
+  end
+
+  def repeat_constant_time_compare(target, guess, count) do
+    constant_time_compare(target, guess)
+    repeat_constant_time_compare(target, guess, count - 1)
+  end
+
+  # Call the private comparison function with no differences accumulated so far.
   def constant_time_compare(a, b) do
     constant_time_compare(a, b, 0)
   end
 
-  def constant_time_compare(<<firsta, resta::binary>>, <<firstb, restb::binary>>, diff) do
+  # XOR the first bytes of the strings to get differences, OR with diff
+  # to accumulate changes.
+  defp constant_time_compare(<<firsta, resta::binary>>, <<firstb, restb::binary>>, diff) do
     constant_time_compare(resta, restb, diff ||| (firsta ^^^ firstb))
   end
 
-  def constant_time_compare(<<>>, <<>>, diff) do
-    diff == 0
-  end
+  # End state: If both strings are empty and diff has not accumulated
+  # any changed bits, the strings where equal, otherwise they are unequal.
+  defp constant_time_compare(<<>>, <<>>, 0), do: :ok
+  defp constant_time_compare(<<>>, <<>>, _diff), do: :error
+  defp constant_time_compare(<<_, _::binary>>, <<>>, _diff), do: :error
+  defp constant_time_compare(<<>>, <<_, _::binary>>, _diff), do: :error
 
 end
